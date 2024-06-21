aa9539569d7761ea6861810329bf556b4046b9f6
Max <mkotsarev40@gmail.com>
2024-06-21 16:10:15 +0200

    Particle Struct

---
+++ particle.h
@@ -0,0 +1,17 @@
+#ifndef H_PARTICLE
+#define H_PARTICLE
+#include "vector.h"
+
+typedef struct Particle {
+    Vec3 position;
+    Vec4 color;
+    float timeToLife;
+} Particle;
+
+void instanciateParticles(Particle* particles) {
+    for () {
+        
+    }
+}
+
+#endif
